0e7d315a27fb02156ff70f56c0b73e3334278c9b
Bruno Carlos Soares <bruno11carloss@gmail.com>
2024-05-22 17:24:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto_BuscaDeFilmes
+ Criando um Buscador de filme com uso de React Vite
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BuscaDeFilmes from './components/BuscadorDeFilmes'
+
+function App() {
+ return (
+    <div>
+      <BuscaDeFilmes/>
+    </div>
+  );
+}
+
+export default App;
